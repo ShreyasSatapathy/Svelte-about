dd7c1ca126e962ebbe95d58ea302c2f0e7b2bb60
ShreyasSatapathy <f20201339@hyderabad.bits-pilani.ac.in>
2022-01-08 08:57:25 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+	;
+</script>
+
+<main>
+	<h1>What is ACM-</h1>
+    <h1>BPHC?</h1>
+	<p>The Association for Computing Machinery is an</p> 
+	<p>	American based research society for computing. With</p> 
+	<p>	over 100,000 student and professional members, it is</p>
+	<p>	 the largest society of its kind in the world.</p>
+    <p>                                  
+	
+	</p>
+	<p>The BITS-Pilani Hyderabad Campus Student Chapter of</p>
+	<p>ACM aims to further ACM's goals of "Advancing</p>
+	<p>Computing as a Science & Profession" in one of the</p>
+	<p>most prestigious engineering colleges in India. Drawing</p>
+	<p>upon ACM's rich history and the knowledge of their</p>
+	<p>thousands of illustrous members, we aim to improve the</p>
+	<p>computing culture on campus through events,</p>
+	<p>workshops, talks and projects.</p>
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img src="C:\Users\shrey\Pictures\ACM-BPHC _ About.html"/>
+</main>
+
+<style>
+	main {
+		text-align: left;
+		padding: 0.1em;
+		max-width: 240px;
+		margin: 0 auto;
+		image-orientation: right;
+	}
+
+	h1 {
+		color: #fdfdfd;
+		text-transform: uppercase;
+		font-size:3em;
+		font-weight:750;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+	:global(body) {
+		background-color: #000;
+		color: #bfc2c7;
+	}
+</style>
